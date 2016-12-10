40afd555df08bc39b9c4d07482f8c66efaa1de61
rolandvs <roland@van-straten.org>
2016-12-10 22:37:34 +0100

    Completed package for adding CLICKER2 to micropython

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # CLICKER2
-Board definition files of micropython for use with Mikroe CLICKER2 board with a STM32F407
+
+date | version | author | Remark
+- | - | - | -
+2016-08-27 | 1.0 | Roland van Straten | initial port
+2016-12-10 | 1.1 | Roland van Straten | Use UART Pin definitions
+
+![clicker2](./documents/clicker2.png)
+
+CLICKER2 is a ```STM32F407VGT6``` based board that features two module connectors, two switches, two LEDs, and a single cell Li-Ion battery charger.
+
+More information: [mikroe clicker2 website](http://www.mikroe.com/stm32/clicker-2/)
+
+## Building firmware
+Following assumes that the path to `micropython` local repository is at `$HOME/workspace/`.
+
+```
+cd $HOME/workspace/micropython/stmhal/boards
+git clone https://github.com/rolandvs/CLICKER2.git
+cd ..
+make BOARD=CLICKER2
+```
+
+
+## Changes to micropython
+
+### Board Specific
+
+Added directories:<br>
+```stmhal/boards/CLICKER2/``` with all board specific files.
+
+```stmhal/boards/CLICKER2/documents/``` with the latest hardware documentation.
+
+```stmhal/boards/CLICKER2/scripts/``` with micropython board support scripts.
+
+### micropython sthal core code
+
+- In the ```Makefile``` the ```USE_PYDFU ?= 0``` is changed from ```1``` to ```0```. This is for convenience using ```deploy``` in the make command.
+
+- The ```stmhal/main.c``` is changed to have a different Flash drive name ```CLICKER2```. The use of ```#define MICROPY_HW_FLASH_LABEL	(1)``` sets the name to the board.
+
+- In the ```stmhal/storage.c``` the extra segment of Flash memory is enabled. This is implemented as ```#define MICROPY_HW_FLASH_EXTRA (1)``` in ```mpconfigboard.h```.
+
+- The left switch ```T2``` is the ```USRSW``` as far as micropython is concerned.
+
+- ```LED1``` and ```LED2``` are defined.
+
+### Patches
+
+#### main.c
+```
+// set label
+// - patched to give flashdrive the name of the board
+#if defined(MICROPY_HW_FLASH_LABEL)
+f_setlabel("/flash/"MICROPY_HW_BOARD_NAME);
+#else
+f_setlabel("/flash/pybflash");
+#endif
+```
+#### storage.c
+```
+// enable this to get an extra 64k of storage (uses the last sector of the flash)
+// - patched for clicker2 board
+//#if 0
+#if defined(MICROPY_HW_FLASH_EXTRA)
+#define FLASH_MEM_SEG2_START_ADDR (0x080e0000) // sector 11
+#define FLASH_MEM_SEG2_NUM_BLOCKS (128) // sector 11: 128k
+#endif
+```
+## Board IO Pins
+
+Board Label | CPU Pin | Description
+- | - | -
+LD1 | PE12 | LED 1 (left), active low
+LD2 | PE15 | LED 2 (right), active low
+SW | PE0 | PYB switch, active low
+T2 | PE0 | left switch, active low
+T3 | PA10 | right switch, active low
+SENSEL | PB12 | select the battery measurement circuit
+VSENSE | PC5  | the battery voltage ADC channel
+FAULT | PC6 | status of the `FAULT` pin, active low
+BATSTAT | PD4 | status of the `CHARGE` pin, active low
+
+## Board Support Package
+A small BSP package is placed in `stmhal/boards/CLICKER2/scripts/` and can be used like:
+
+```
+import bsp
+clicker2 = bsp.BSP()
+```
+Some code to exercise the functions is executed through `execfile('bsp.py')`.
+
+The functions are:
+
+### ReadBatteryVoltage()
+The battery voltage will return the voltage measured at the battery using a voltage divider.
+
+```
+batval = clicker2.ReadBatteryVoltage()
+print("Battery voltage is: {:4.2f}.".format(batval*9.9/4096))
+```
+### ReadChargerStatus()
+This reads the two status pins ```/CHARGE``` and ```/FAULT``` of the power supply and returns them.
+
+### IsChargerActive()
+Returns ```True``` if charging.
+
+### IsFaultActive()
+Returns ```True``` if power supply generates a fault signal.
+
+### IsButtonxPressed()
+Return ```True``` if button is pressed when calling ```IsButton1Pressed()``` or ```IsButton2Pressed()```.
---
+++ mpconfigboard.h
@@ -0,0 +1,147 @@
+/*
+  mpconfigboard.h
+
+  for the CLICKER2 board
+  
+*/
+#define MICROPY_HW_BOARD_NAME       "CLICKER2"
+#define MICROPY_HW_MCU_NAME         "STM32F407VGT6"
+
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (1)
+#define MICROPY_HW_ENABLE_CAN       (1)
+
+#define HSE_VALUE    				((uint32_t)25000000)
+// HSE is 25MHz
+#define MICROPY_HW_CLK_PLLM 		(25)
+#define MICROPY_HW_CLK_PLLN 		(336)
+#define MICROPY_HW_CLK_PLLP 		(RCC_PLLP_DIV2)
+#define MICROPY_HW_CLK_PLLQ 		(7)
+//#define MICROPY_HW_CLK_LAST_FREQ (1)				// -> what does it mean?
+
+// The pyboard has a 32kHz crystal for the RTC
+#define MICROPY_HW_RTC_USE_LSE      (1)
+#define MICROPY_HW_RTC_USE_US       (0)
+#define MICROPY_HW_RTC_USE_CALOUT   (1)
+
+// CAN busses
+#define MICROPY_HW_CAN1_NAME "CAN1" // CAN1 on RX,TX = Y3,Y4 = PB8,PB9
+#define MICROPY_HW_CAN2_NAME "CAN2" // CAN2 on RX,TX = Y5,Y6 = PB12,PB13
+
+// some extra init code goes in separate module
+#define MICROPY_BOARD_EARLY_INIT	(0)
+
+// REPL on second port
+//#define MICROPY_HW_UART_REPL 		(1)				// port accessible through tag connect port
+//#define MICROPY_HW_UART_REPL_BAUD	(115200)		// fast enough
+
+// not used now
+//#define MICROPY_HW_UART1_TX     (pin_B6)
+//#define MICROPY_HW_UART1_RX     (pin_B7)
+
+// on MB1, no handshake used
+#define MICROPY_HW_UART2_TX     (pin_D5)
+#define MICROPY_HW_UART2_RX     (pin_D6)
+//#define MICROPY_HW_UART2_RTS    (pin_A1)
+//#define MICROPY_HW_UART2_CTS    (pin_A0)
+
+// on MB2, no handshake support
+#define MICROPY_HW_UART3_TX     (pin_D8)
+#define MICROPY_HW_UART3_RX     (pin_D9)
+//#define MICROPY_HW_UART3_RTS    (pin_B14)
+//#define MICROPY_HW_UART3_CTS    (pin_B13)
+
+// on HDR2
+#define MICROPY_HW_UART4_TX     (pin_A0)
+#define MICROPY_HW_UART4_RX     (pin_A1)
+
+#define MICROPY_HW_UART6_TX     (pin_C6)
+#define MICROPY_HW_UART6_RX     (pin_C7)
+
+// I2C busses
+//#define MICROPY_HW_I2C1_SCL (pin_B6)
+//#define MICROPY_HW_I2C1_SDA (pin_B7)
+
+#define MICROPY_HW_I2C2_SCL (pin_B10)	// on MB2
+#define MICROPY_HW_I2C2_SDA (pin_B11)
+
+#define MICROPY_HW_I2C3_SCL (pin_A8)	// on MB1
+#define MICROPY_HW_I2C3_SDA (pin_C9)
+
+// SPI busses
+//#define MICROPY_HW_SPI1_NSS  (pin_A4)
+//#define MICROPY_HW_SPI1_SCK  (pin_A5)
+//#define MICROPY_HW_SPI1_MISO (pin_A6)
+//#define MICROPY_HW_SPI1_MOSI (pin_A7)
+
+#define MICROPY_HW_SPI2_SCK  (pin_B13)	// on MB 2
+#define MICROPY_HW_SPI2_MISO (pin_B14)
+#define MICROPY_HW_SPI2_MOSI (pin_B15)
+#define MICROPY_HW_SPI2_NSS  (pin_E11)
+
+#define MICROPY_HW_SPI3_SCK  (pin_C10)	// on MB 1
+#define MICROPY_HW_SPI3_MISO (pin_C11)
+#define MICROPY_HW_SPI3_MOSI (pin_C12)
+#define MICROPY_HW_SPI3_NSS  (pin_E8)
+
+
+// T2-PE0 - USRSW is pulled high. Pressing the button makes the input go low.
+//
+#define MICROPY_HW_USRSW_PIN        (pin_E0)
+#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
+
+// T3-PA10 - Second Switch, normally pulled high, Pressing button makes the input low.
+// -- not implemented yet
+#define MICROPY_HW_USRSW2_PIN        (pin_A10)
+
+// LEDs
+//
+#define MICROPY_HW_LED1             (pin_E12) // LED 1 (red)
+#define MICROPY_HW_LED2             (pin_E15) // LED 2
+//#define MICROPY_HW_LED3             (pin_E15)
+//#define MICROPY_HW_LED4             (pin_E15)
+
+// CHECK IF THESE PINS CAN BE USED FOR PWM
+#define MICROPY_HW_LED1_PWM         { TIM2, 2, TIM_CHANNEL_1, GPIO_AF1_TIM2 }
+#define MICROPY_HW_LED2_PWM         { TIM3, 3, TIM_CHANNEL_1, GPIO_AF2_TIM3 }
+
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_low(pin))
+
+// SD card detect switch
+//
+#if MICROPY_HW_HAS_SDCARD
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (1)
+#endif
+
+// USE EXTRA 64K IN FLASH
+#define MICROPY_HW_FLASH_EXTRA	(1)	// use the extra 64K in stmhal/storage.c when defined
+// SET LABEL OF DRIVE TO BOARD NAME
+#define MICROPY_HW_FLASH_LABEL	(1)	// set the label of the drive in stmhal/main.c when defined
+
+// USB config
+#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
+
+// NOT USED
+//
+// networking
+#define MICROPY_PY_NETWORK			(0)		// added NETWORK ENABLING
+#define MICROPY_PY_USOCKET			(0)		// added SOCKET NEEDS NETWORK OTHERWISE LINK ERROR
+
+// MMA accelerometer config
+//#define MICROPY_HW_MMA_AVDD_PIN     (pin_B5)
