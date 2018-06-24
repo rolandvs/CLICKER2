78e2d40b9ee46eab40bd689d7a266983d21fe6b7
roland <roland@van-straten.org>
2018-06-24 19:27:17 +0200

    Just to make sure

--- stm32f4xx_hal_conf.h
+++ stm32f4xx_hal_conf.h
@@ -46,8 +46,6 @@
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 
-#define USE_USB_FS
-
 /* ########################## Module Selection ############################## */
 /**
   * @brief This is the list of modules to be used in the HAL driver
@@ -71,7 +69,7 @@
 /* #define HAL_HASH_MODULE_ENABLED */
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_I2C_MODULE_ENABLED
-#define HAL_I2S_MODULE_ENABLED
+/* #define HAL_I2S_MODULE_ENABLED */
 /* #define HAL_IWDG_MODULE_ENABLED */
 /* #define HAL_LTDC_MODULE_ENABLED */
 #define HAL_PWR_MODULE_ENABLED
@@ -99,8 +97,7 @@
   *        (when HSE is used as system clock source, directly or through the PLL).
   */
 #if !defined  (HSE_VALUE)
-  #define HSE_VALUE   ((uint32_t)25000000)  /*!< Value of the External oscillator in Hz */
-  //#define HSE_VALUE   ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
+  #define HSE_VALUE    ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #if !defined  (HSE_STARTUP_TIMEOUT)
