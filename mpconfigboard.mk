a920a7d314a172c467ad03d29b8a3fdc53e27c93
roland <roland@van-straten.org>
2018-06-24 19:22:19 +0200

    Added new linker file and frozen variability

--- mpconfigboard.mk
+++ mpconfigboard.mk
@@ -1,4 +1,9 @@
 MCU_SERIES = f4
 CMSIS_MCU = STM32F407xx
 AF_FILE = boards/stm32f405_af.csv
-LD_FILE = boards/stm32f405.ld
+LD_FILES = boards/CLICKER2/stm32f407.ld boards/common_ifs.ld
+TEXT0_ADDR = 0x08000000
+TEXT1_ADDR = 0x08020000
+
+# Select if default frozen modules needs to included
+FROZEN_MPY_DIR ?=
